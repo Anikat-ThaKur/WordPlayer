7c28017357368cc3ec3c44a96510350d176a5f50
Anikat-ThaKur <anikatthakur1605@gmail.com>
2022-08-08 13:06:25 +0530

    Updates

---
+++ precache-manifest.6425d520c04001f89fc38bc49545ec1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c023cdb8a8ae2199aede",
+    "url": "/WordPlayer/static/css/main.a59d8f14.chunk.css"
+  },
+  {
+    "revision": "c023cdb8a8ae2199aede",
+    "url": "/WordPlayer/static/js/main.c023cdb8.chunk.js"
+  },
+  {
+    "revision": "281e318900103c44d5c0",
+    "url": "/WordPlayer/static/js/1.281e3189.chunk.js"
+  },
+  {
+    "revision": "92d49ecc5adc2423f05d",
+    "url": "/WordPlayer/static/js/2.92d49ecc.chunk.js"
+  },
+  {
+    "revision": "645c8a663cfe8da1b3e2",
+    "url": "/WordPlayer/static/js/runtime~main.645c8a66.js"
+  },
+  {
+    "revision": "1444a236714bd29b78904c7a03d0e20c",
+    "url": "/WordPlayer/index.html"
+  }
+];
